842e07d8acf30f9582dd620d249ca13da1e48aa0
Chase Wickett <wickettc@yahoo.com>
2021-03-18 14:54:57 -0400

    fixed bug with friend requests

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -26,7 +26,6 @@ const Profile = ({ match, token }) => {
     useEffect(() => {
         async function fetchUser(id, token) {
             const res = await getUserProfile(id, token);
-            console.log(res.data);
             setCurProfile(res.data);
             setFriends(res.data.friends);
             setFriendRequests(res.data.friendrequests);
@@ -39,14 +38,17 @@ const Profile = ({ match, token }) => {
         if (token.user.friends.includes(curProfile._id)) {
             setIsFriend(true);
         }
-        if (
-            curProfile.friendrequests
-            // curProfile.friendrequests.includes(token.user._id)
-        ) {
+        if (curProfile.friendrequests) {
             curProfile.friendrequests.filter((req) => {
                 return req._id === token.user._id
                     ? setIsFriendPending(true)
                     : null;
+            });
+        }
+        // prevents user from sending request to someone who already requested them
+        if (token.user.friendrequests && !_.isEmpty(curProfile)) {
+            token.user.friendrequests.filter((req) => {
+                return req === curProfile._id ? setIsFriendPending(true) : null;
             });
         }
     }, [curProfile, token]);
@@ -85,28 +87,31 @@ const Profile = ({ match, token }) => {
     return (
         <div>
             {console.log('curprofile', curProfile)}
-            {console.log('isfriend', isFriend)}
-            {console.log('ismyprofile', isMyProfile)}
-            {console.log('friends,', friends)}
-            {console.log('friend requests,', friendRequests)}
+            {console.log('token', token)}
+            {/* {console.log('isfriend', isFriend)} */}
+            {/* {console.log('friends,', friends)} */}
+            {/* {console.log('friend requests,', friendRequests)} */}
             {_.isEmpty(curProfile) ? (
                 <div className="loading"></div>
             ) : (
                 <div>
                     <h1>
+                        {isMyProfile ? 'YOUR PROFILE' : null}{' '}
                         {curProfile.firstname} {curProfile.lastname}
                     </h1>
+
                     {/* ADD FRIEND */}
                     {isMyProfile ? null : isFriend ? (
                         <h3>You are friends!</h3>
                     ) : isFriendPending ? (
-                        <h3>Friend Request Sent!</h3>
+                        <h3>Friend Request Pending!</h3>
                     ) : (
                         <button onClick={handleSendFriendRequest}>
                             Add Friend
                         </button>
                     )}
                     {/* ADD FRIEND */}
+
                     {/* FRIEND REQUESTS */}
                     {isMyProfile ? <h3>Friend Requests</h3> : null}
                     {isMyProfile && _.isEmpty(friendRequests) ? (
